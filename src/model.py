c830f788cc7d01d30d212631c89b32d8fc88b186
GovindaKandel <govindkandel09@gmail.com>
2026-02-06 23:13:18 +0545

    update

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegression
 import joblib
 import os
 
@@ -7,6 +8,12 @@ def train_nb(X_train, y_train):
     model = MultinomialNB()
     model.fit(X_train, y_train)
     return model
+
+def train_logistic_regression(X_train, y_train):
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+    return model
+
 
 #  Save trained model to models directory
 def save_model(model, filename='spam_classifier.pkl'):
